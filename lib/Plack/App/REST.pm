f969459a136ca17d080b737980836c82f66d7e19
Vaclav Dovrtel <vaclav.dovrtel@gmail.com>
2015-05-20 19:00:23 +0200

    0.03

--- lib/Plack/App/REST.pm
+++ lib/Plack/App/REST.pm
@@ -4,7 +4,7 @@ use 5.008_005;
 use strict;
 use warnings FATAL => 'all';
 
-our $VERSION = '0.02'; # Set automatically by milla
+our $VERSION = '0.03'; # Set automatically by milla
 
 use parent qw( Plack::Component );
 use HTTP::Exception;
